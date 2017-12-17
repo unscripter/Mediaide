61cd797734cf733c1561738705e3177a0ad8eba2
Amit Mundra <amit.mundra@mediassistindia.com>
2017-12-18 03:32:39 +0530

    add header

--- src/app/shared/header/header.ts
+++ src/app/shared/header/header.ts
@@ -2,7 +2,8 @@ import { Component } from '@angular/core';
 
 @Component({
     selector: 'header',
-    templateUrl: './header.html'
+    templateUrl: './header.html',
+    styleUrls: ['./header.css']
 })
 
 export class Header {
