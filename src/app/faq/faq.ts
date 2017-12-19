2b464cf6546069e4c2d3dba9dea47ba72c7af954
Amit Mundra <amit.mundra@mediassistindia.com>
2017-12-19 23:29:08 +0530

    faq page creation

---
+++ src/app/faq/faq.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { NgbAccordion } from '@ng-bootstrap/ng-bootstrap';
+
+@Component({
+  selector: 'frequenty-asked-questions',
+  templateUrl: './faq.html',
+  styleUrls: ['./faq.css']
+})
+
+export class FAQ {
+
+} 
